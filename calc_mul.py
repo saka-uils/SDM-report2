8166fbc070a9cbe44b9e8e5a36e5e880d927c303
saka-uils <saintsogt.uilssaruul.da@tut.jp>
2026-02-05 20:33:45 +0900

    Fix calc() to pass all test cases

--- calc_mul.py
+++ calc_mul.py
@@ -2,26 +2,17 @@
 
 import re
                 
-def calc(A,B):
-        ai=str(A)
-        bi=str(B)
-        p = re.compile('\d+(\.\d+)?')
-        if p.match(ai) or p.match(bi):
-                a=float(ai)
-                b=float(bi)
-                if 0<a and a<b and b<1000:
-                        valid=True
-                else:
-                        valid=False
-        else:
-                valid=False
-                
-        if valid:
-                ans=a*b
-                return ans
-        else:
+      
+def calc(A, B):
+        # type check
+        if not isinstance(A, int) or not isinstance(B, int):
                 return -1
-        
+
+        # range check
+        if A < 1 or A > 999 or B < 1 or B > 999:
+                return -1
+
+        return A * B
                 
 def main ():
 	matchstring = ''
